bf8986c19b8f1ef7337421e947830333fce777db
Olegario, Ralph Huey Zapatero <ro68@students.waikato.ac.nz>
2022-05-16 16:50:23 +1200

    Hello

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -3,12 +3,20 @@
 echo "(0) Prepare environment"
 
 echo "(1) Build (compiling the application)"
-  if npm run build; then
+  if npm run build;
+  then
     git add .
-    git commit -m "Successful commit"
-    git push --force
+    if [ "$1" -eq 0 ]
+    then
+      git commit -m "COMPX341-22A-A3 Committing from CI/CD Pipeline"
+    else
+      echo "$1"
+      git commit -m "$1"
+    fi
+    git push --force git@github.com:TikiTikiPlus/AWSDevOps.git
     echo "build succeeded"
   else
     echo "build failed"
+
   fi
 echo "(2) Tests"
