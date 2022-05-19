13ea30f4a0f000bddb67b11ce9d13ddd7d634bdd
Olegario, Ralph Huey Zapatero <ro68@students.waikato.ac.nz>
2022-05-19 17:48:13 +1200

    COMPX341-22A-A3 Committing from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -2,7 +2,7 @@
 
 echo "(0) Prepare environment"
 echo "(1) Build (compiling the application)"
-  npm install
+  #npm install
   if npm run build;
   then
     echo "build succeeded"
@@ -11,11 +11,11 @@ echo "(1) Build (compiling the application)"
     if [ $# -eq 0 ]
     then
       git commit -m "COMPX341-22A-A3 Committing from CI/CD Pipeline"
+      echo "COMPX341-22A-A3 Committing from CI/CD Pipeline"
     else
       echo "$1"
       git commit -m "$1"
     fi
-
 
   else
     echo "build failed"
